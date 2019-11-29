fd42597c212cf36c7a51f7ac27bb5e6a4a4f1aca
confident-history-3187 <boris.matovic@gmail.com>
2019-11-29 19:57:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var Object.assign(newObj, object)
+  var newObj
+  Object.assign(newObj, object)
   delete newObj[key]
   return newObj
 }
