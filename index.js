76891206f235334c6fd3d90f230b9d4d4bd76be0
confident-history-3187 <boris.matovic@gmail.com>
2019-11-29 19:54:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateObjectWithKeyAndValue(object, key, value){
     return Object.assign(newObj, object)
 }
 
-function destructivelyUpdateObjectwithkeyAndValue(object, key, value){
+function destructivelyUpdateObjectWithkeyAndValue(object, key, value){
   object[key] = value
   return object
 }
